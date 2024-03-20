1325d22962563a2b2c1ed4df56a9e2813e1a888b
Izaaq <odongoizaaq@gmail.com>
2024-03-20 14:34:05 -0400

    Adds arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+let num1 = 31
+let num2 =2
+let multiply = (num1 * num2)
+console.log(multiply)
+
+let num3 =19
+let num4 = 5
+let mod =(num3 % num4)
+console.log(mod)
+
+let max= (1,2,3,4,20)
+Math.max(max);
+console.log(max)
+
+let number = 0
+let random = (number+1)
+console.log(random)
+
